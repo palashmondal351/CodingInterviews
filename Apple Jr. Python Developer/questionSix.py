ff84945c5d998e4eb5a69036c31676e4fa6eb171
Palash Mondal <42488206+palashmondal351@users.noreply.github.com>
2019-05-16 15:00:35 +0530

    optimize the complexity of the code
    user input and others variable can be optimize  it will become simple.

--- Apple Jr. Python Developer/questionSix.py
+++ Apple Jr. Python Developer/questionSix.py
@@ -1,36 +1,23 @@
-import sys
-
-
-def solution(actual_price, given_price):
-    if given_price < actual_price:
-        print("ERROR")
+# Apple junier python developer
+ap,gp=map(float,input().split(';'))
+paise={.01:'PENNY', .05:'NICKEL', .10:'DIME', .25:'QUATER', .50:'HALF HOLLAR',
+       1:'ONE', 2:'TWO', 5:'FIVE', 10:'TEN', 20:'TWENTY', 50:'FIFTY', 100:'ONE HUNDED'}
+value=[100,50,20,10,5,2,1,.50,.25,.10,.05,.01]
+def result(ap,gp):
+    if(ap>gp):
+        print('ERROR')
         return
-    elif given_price == actual_price:
-        print("ZERO")
+    elif(ap==gp):
+        print('NONE')
         return
     else:
-        price_to_return = given_price - actual_price
-        results = set([])
-
-        for value in values:
-            while price_to_return >= value:
-                results.add(value)
-                price_to_return = price_to_return - value
-
-        results = list(results)
-        results = [name2value[result] for result in results]
-
-        results = sorted(results)
-        print(",".join(results))
+        rval=gp-ap
+        l=[]
+        for val in value:
+            if(rval>=val):
+                l.append(val)
+                rval-=val
+        reamt=sorted([paise[k] for k in l])
+        print(','.join(reamt))
         return
-
-
-name2value = {.01: 'PENNY', .05: 'NICKEL', .10: 'DIME', .25: 'QUARTER', .50: 'HALF DOLLAR', 1.00: 'ONE', 2.00: 'TWO',
-              5.00: 'FIVE', 10.00: 'TEN', 20.00: 'TWENTY', 50.00: 'FIFTY'}
-
-values = [100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05, 0.01]
-
-for line in sys.stdin:
-    purchase_price, cash = line.split(";")
-    purchase_price, cash = float(purchase_price), float(cash)
-    solution(purchase_price, cash)
+result(ap,gp)                
